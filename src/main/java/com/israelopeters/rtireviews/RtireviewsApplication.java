7266ad18528afef9a88bd21ca31b9d91e29d86c0
israelopeters <israelopeters@gmail.com>
2024-10-19 20:20:51 +0100

    Intitialize Spring Boot application locally

---
+++ src/main/java/com/israelopeters/rtireviews/RtireviewsApplication.java
@@ -0,0 +1,13 @@
+package com.israelopeters.rtireviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtireviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtireviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelopeters/rtireviews/RtireviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelopeters.rtireviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtireviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
