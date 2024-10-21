6666a99d5bab3327909b4093c2596be84b37713d
israelopeters <israelopeters@gmail.com>
2024-10-21 23:05:45 +0100

    feat: Create User entity

---
+++ src/main/java/com/israelopeters/rtireviews/model/User.java
@@ -0,0 +1,32 @@
+package com.israelopeters.rtireviews.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity(name = "Users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String country;
+
+    private String bio;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate dateCreated;
+}
