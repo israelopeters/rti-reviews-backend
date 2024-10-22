9d794093de16c2c899f171a51e5268c6c2be7bcb
israelopeters <israelopeters@gmail.com>
2024-10-22 21:11:43 +0100

    refactor(model): Update UserRepository

--- src/main/java/com/israelopeters/rtireviews/repository/UserRepository.java
+++ src/main/java/com/israelopeters/rtireviews/repository/UserRepository.java
@@ -2,7 +2,14 @@ package com.israelopeters.rtireviews.repository;
 
 import com.israelopeters.rtireviews.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    List<User> findByDate(LocalDate date);
 }
