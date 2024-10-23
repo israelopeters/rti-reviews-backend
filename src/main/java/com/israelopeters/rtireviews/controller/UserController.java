8980a2e495485137efac206858964c70aa3872a3
israelopeters <israelopeters@gmail.com>
2024-10-23 03:08:00 +0100

    feat(controller): Create get mapping for all users

---
+++ src/main/java/com/israelopeters/rtireviews/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.israelopeters.rtireviews.controller;
+
+import com.israelopeters.rtireviews.model.User;
+import com.israelopeters.rtireviews.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+}
