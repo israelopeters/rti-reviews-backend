a325f60eb9fa28849dbe4ce9643f0269b929a9b3
israelopeters <israelopeters@gmail.com>
2024-10-24 08:52:13 +0100

    feat: Create UserDto model

---
+++ src/main/java/com/israelopeters/rtireviews/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.israelopeters.rtireviews.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDto {
+
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    @Email(message = "Email cannot be empty")
+    private String email;
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+}
