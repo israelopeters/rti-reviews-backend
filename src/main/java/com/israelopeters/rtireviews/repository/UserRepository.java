fe1a183f06e475e9680a04f414ed6d5ff0592509
israelopeters <israelopeters@gmail.com>
2024-10-29 01:14:20 +0000

    refactor(service): Refactor dependency injection

--- src/main/java/com/israelopeters/rtireviews/repository/UserRepository.java
+++ src/main/java/com/israelopeters/rtireviews/repository/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
-    List<User> findByDate(LocalDate date);
+    List<User> findByDateCreated(LocalDate date);
 }
