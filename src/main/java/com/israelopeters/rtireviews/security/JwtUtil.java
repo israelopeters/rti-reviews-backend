fb772452061f1ed978c49e59bf4a3d8aeaceee9e
israelopeters <israelopeters@gmail.com>
2024-11-01 23:35:29 +0000

    feat: Create class for JWT

---
+++ src/main/java/com/israelopeters/rtireviews/security/JwtUtil.java
@@ -0,0 +1,11 @@
+package com.israelopeters.rtireviews.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUtil {
+
+    private String SECRET_KEY = "secrete_key";
+
+
+}
