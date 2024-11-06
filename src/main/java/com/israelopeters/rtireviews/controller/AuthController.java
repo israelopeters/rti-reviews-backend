13f82180c05a65bd2b9ad7d31d8c6eb1c361b817
israelopeters <israelopeters@gmail.com>
2024-11-06 23:10:21 +0000

    feat(controller): Create handler method to persist created account

--- src/main/java/com/israelopeters/rtireviews/controller/AuthController.java
+++ src/main/java/com/israelopeters/rtireviews/controller/AuthController.java
@@ -1,14 +1,23 @@
 package com.israelopeters.rtireviews.controller;
 
-import com.israelopeters.rtireviews.dto.UserDto;
+import com.israelopeters.rtireviews.model.User;
+import com.israelopeters.rtireviews.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // Controller for Spring MVC test purposes
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/home")
     public String home() {
@@ -17,9 +26,30 @@ public class AuthController {
 
     @GetMapping("/signup")
     public String showSignupForm(Model model) {
-        UserDto user = new UserDto();
+        User user = new User();
         model.addAttribute("userForSignup", user);
         return "signup";
+    }
+
+    @PostMapping("/signup/create")
+    public String signup(@Valid @ModelAttribute("userForDto") User user, BindingResult bindingResult, Model model) {
+        if(isUserExists(user)) {
+            bindingResult.rejectValue(
+                    "email", "", "This email is already registered to a user.");
+        }
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("userForSignup", user);
+            return "/signup";
+        }
+        userService.addUser(user);
+        return "redirect:/signup?success";
+    }
+
+    private Boolean isUserExists(User user) {
+        User existingUser = userService.getUserByEmail(user.getEmail());
+        return (existingUser != null &&
+                existingUser.getEmail() != null &&
+                !existingUser.getEmail().isEmpty());
     }
 
 
