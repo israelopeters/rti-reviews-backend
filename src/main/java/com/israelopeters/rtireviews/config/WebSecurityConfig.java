9e2ec35c89da0ee57fc5e7d16b4cbf05226e1b3b
israelopeters <israelopeters@gmail.com>
2024-11-06 00:45:03 +0000

    refactor(config): Make signup page accessible without authentication

--- src/main/java/com/israelopeters/rtireviews/config/WebSecurityConfig.java
+++ src/main/java/com/israelopeters/rtireviews/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "home").permitAll()
+                        .requestMatchers("/", "/home", "/signup").permitAll()
                         .anyRequest().authenticated())
                 .formLogin((form) -> form
                         .loginPage("/login")
