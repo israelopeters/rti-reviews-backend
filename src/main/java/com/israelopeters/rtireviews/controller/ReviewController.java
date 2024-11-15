b33deff91aa3075f670b5a494761a412fc766585
israelopeters <israelopeters@gmail.com>
2024-11-15 05:48:03 +0000

    feat(controller): Create method to add review in api endpoint

--- src/main/java/com/israelopeters/rtireviews/controller/ReviewController.java
+++ src/main/java/com/israelopeters/rtireviews/controller/ReviewController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -22,5 +24,12 @@ public class ReviewController {
     @GetMapping("/")
     public ResponseEntity<List<Review>> getAllReviews() {
         return new ResponseEntity<>(reviewService.getAllReviews(), HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<Review> addReview(@RequestBody Review review) {
+        review.setDateTimeCreated(LocalDateTime.now());
+        reviewService.addReview(review);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
