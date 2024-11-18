946383b84e71d617c0d474829e3352e72713ded2
israelopeters <israelopeters@gmail.com>
2024-11-18 23:08:24 +0000

    feat(controller): Create endpoint to edit review

--- src/main/java/com/israelopeters/rtireviews/controller/ReviewController.java
+++ src/main/java/com/israelopeters/rtireviews/controller/ReviewController.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -28,8 +24,13 @@ public class ReviewController {
 
     @PostMapping("/post")
     public ResponseEntity<Review> addReview(@RequestBody Review review) {
-        review.setDateTimeCreated(LocalDateTime.now());
         reviewService.addReview(review);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Review> editReview(@PathVariable Long id, @RequestBody Review updatedReview) {
+        reviewService.editReview(id, updatedReview);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
