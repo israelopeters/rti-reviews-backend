6f92a43f696e15d7eb30d10bf1048aec23605421
israelopeters <israelopeters@gmail.com>
2024-11-22 13:14:21 +0000

    chore(repository): Remove unused import statements

--- src/main/java/com/israelopeters/rtireviews/repository/UserRepository.java
+++ src/main/java/com/israelopeters/rtireviews/repository/UserRepository.java
@@ -4,9 +4,6 @@ import com.israelopeters.rtireviews.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
