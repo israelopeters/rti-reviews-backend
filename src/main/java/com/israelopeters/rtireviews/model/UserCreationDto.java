b598249f7f8307527782e90569b1e4121780e7a9
israelopeters <israelopeters@gmail.com>
2025-01-17 22:03:26 +0000

    feat: Create UserCreationDto class

---
+++ src/main/java/com/israelopeters/rtireviews/model/UserCreationDto.java
@@ -0,0 +1,25 @@
+package com.israelopeters.rtireviews.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreationDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String country;
+
+    private String bio;
+
+    private String email;
+
+    private String password;
+}
